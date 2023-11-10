f041b0d9bf36ac74acf3c9d70eaaec5794a12de9
azambrao02 <a.zambrano02@ufromail.cl>
2023-11-10 10:09:07 -0300

    Creacion de los metodos de cada clase y sus constructores

--- src/main/java/Atleta.java
+++ src/main/java/Atleta.java
@@ -5,56 +5,54 @@ public class Atleta {
 	private String nombre;
 	private String pais;
 	private int edad;
-	private List<Disciplina> listaDisciplinas;
-	private Equipo equipo;
-	private ArrayList<Equipo> equipos = new ArrayList<Equipo>();
-	private ArrayList<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	private List<Equipo> equipos;
+	private List<Disciplina> disciplinas;
 
 	public String getNombre() {
 		return this.nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getPais() {
 		return this.pais;
 	}
-
 	public void setPais(String pais) {
 		this.pais = pais;
 	}
-
 	public int getEdad() {
 		return this.edad;
 	}
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
-	public List<Disciplina> getListaDisciplinas() {
-		throw new UnsupportedOperationException();
+	public List<Equipo> getEquipos(){
+		return this.equipos;
 	}
-
-	public void setListaDisciplinas(List<Disciplina> listaDisciplinas) {
-		throw new UnsupportedOperationException();
+	public void setEquipos(List<Equipo> equipos) {
+		this.equipos = equipos;
+	}
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
 	}
 
 	public void agregarDisciplina(Disciplina disciplina) {
-		throw new UnsupportedOperationException();
+		disciplinas.add(disciplina);
 	}
-
 	public void eliminarDisciplina(Disciplina disciplina) {
-		throw new UnsupportedOperationException();
+		disciplinas.remove(disciplina);
 	}
-
 	public String toString() {
-		throw new UnsupportedOperationException();
+		return "Nombre: " + nombre +",Pais: " +pais+ ",Edad: " +edad+ ",Equipos: " +equipos+ ",Disciplinas: " + disciplinas;
 	}
-
 	public Atleta(String nombre, String pais, int edad) {
-		throw new UnsupportedOperationException();
+		this.nombre = nombre;
+		this.pais = pais;
+		this.edad = edad;
+		this.equipos = new ArrayList<>();
+		this.disciplinas = new ArrayList<>();
 	}
 }
--- src/main/java/Disciplina.java
+++ src/main/java/Disciplina.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Disciplina {
@@ -6,48 +7,43 @@ public class Disciplina {
 	private String recordMundial;
 	private List<Atleta> atletas;
 	private Evento evento;
+	private Disciplina disciplinaEqupos;
 
 	public String getNombre() {
 		return this.nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public int getNumDeParticipantes() {
 		return this.numDeParticipantes;
 	}
-
 	public void setNumDeParticipantes(int numDeParticipantes) {
 		this.numDeParticipantes = numDeParticipantes;
 	}
-
 	public String getRecordMundial() {
 		return this.recordMundial;
 	}
-
 	public void setRecordMundial(String recordMundial) {
 		this.recordMundial = recordMundial;
 	}
+	public List<Atleta> getAtletas() {
+		return this.atletas;
+	}
+	public void setAtletas(List<Atleta> atletas) {
+		this.atletas = atletas;
+	}
+	public Disciplina getDisciplinaEquipo() {
+		return this.disciplinaEqupos;
+	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+		return "Nombre: " +nombre+ "Numero de participantes: " + numDeParticipantes + "El record mundial actual: " + recordMundial;
 	}
-
-	public List<Atleta> getAtletas() {
-		throw new UnsupportedOperationException();
-	}
-
-	public void setAtletas(List<Atleta> atletas) {
-		throw new UnsupportedOperationException();
-	}
-
-	public Disciplina getDisciplinaEquipo() {
-		throw new UnsupportedOperationException();
-	}
-
 	public Disciplina(String nombre, int numDeParticipantes, String recordMundial) {
-		throw new UnsupportedOperationException();
+		this.nombre = nombre;
+		this.numDeParticipantes = numDeParticipantes;
+		this.recordMundial = recordMundial;
+		this.atletas = new ArrayList<>();
 	}
 }
